fbb2575e5ac77d5a5dfd1d6799a8a7db6412ec89
Siphelele <sphllzulu@gmail.com>
2024-12-30 19:57:07 +0200

    removed the bottom nav

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     <div>
       <Navbar/>
       <Tbl/>
-      <footer> <BottomNavbarSmall/></footer>
+      {/* <footer> <BottomNavbarSmall/></footer> */}
      
       
     </div>
