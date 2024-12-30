ce41f62da9af9eb8b3f1acd3b24759d414ac9af8
Siphelele <sphllzulu@gmail.com>
2024-12-30 19:54:54 +0200

    attempted to make the bottom navbar remain at the bottom but i failed

--- src/BottomNavbarSmall.jsx
+++ src/BottomNavbarSmall.jsx
@@ -1,13 +1,9 @@
 import React, { useState, useEffect } from 'react';
-
 import { Link } from 'react-router-dom';
-import './BottomNavbarSmall.css'
-
+import './BottomNavbarSmall.css';
 import { FaHome } from "react-icons/fa";
 import { MdFolderDelete } from "react-icons/md";
 import { GrStatusGood } from "react-icons/gr";
-
-
 
 function BottomNavbarSmall() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
@@ -22,20 +18,17 @@ function BottomNavbarSmall() {
   return (
     isMobile && (
       <nav className="bottom-navbar">
-        
         <ul className="list">
           <li>
-            <Link to="/home"><FaHome style={{fontSize:'50px'}}/></Link>
-          </li>
-          
-          <li>
-            <Link to="/active"><GrStatusGood  style={{fontSize:'50px'}}/></Link>
+            <Link to="/home"><FaHome style={{ fontSize: '24px' }} /></Link>
           </li>
           <li>
-            <Link to="/removed"><MdFolderDelete  style={{fontSize:'50px'}}/></Link>
+            <Link to="/active"><GrStatusGood style={{ fontSize: '24px' }} /></Link>
+          </li>
+          <li>
+            <Link to="/removed"><MdFolderDelete style={{ fontSize: '24px' }} /></Link>
           </li>
         </ul>
-        
       </nav>
     )
   );
